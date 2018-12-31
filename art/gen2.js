74e920bf06c692221371da2dd3b1ca451a083210
ip999 <porteous@gmail.com>
2018-12-31 15:19:12 +0000

    update file structure

---
+++ art/gen2.js
@@ -0,0 +1,7 @@
+for (i = 0; i < 1000; i++) {
+    if (i % 2 == 0){
+        console.log("a\n".repeat(i));
+    } else {
+        console.log("b\n".repeat(i));
+    }
+}
---
+++ art/gen2.py
@@ -0,0 +1,7 @@
+
+for i in range(0, 1000):
+    if (i % 2 == 0):
+            print("a\n"*i)
+            
+    if (i % 2 != 0):
+            print("b\n"*i)
